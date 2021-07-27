9c6b49a520633c418c60b45dec6b41f8c8196b09
flaaaps <julian@wening.me>
2021-07-27 12:08:58 +0200

    Redirect to spotify

---
+++ pages/api/auth/index.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export const redirectURI =
+    process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/auth/callback' : ''
+
+export default function handler(_: NextApiRequest, res: NextApiResponse) {
+    res.redirect(
+        `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${redirectURI}`
+    )
+}
