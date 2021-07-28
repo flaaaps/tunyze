381c4e34fe9ba4b7ca2c57075e920296c47305b4
flaaaps <julian@wening.me>
2021-07-28 13:30:42 +0200

    Add type for returned auth data & save auth data in cookie

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -2,12 +2,20 @@ import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { redirectURI } from '.'
 import qs from 'qs'
+import { serialize } from 'cookie'
+
+type TokenData = {
+    access_token: string
+    refresh_token: string
+    token_type: string
+    expires_in: number
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const code = req.query.code
     if (!req.query.error && code) {
         try {
-            const tokenGrantData = (
+            const tokenGrantData: TokenData = (
                 await axios.post(
                     'https://accounts.spotify.com/api/token',
                     qs.stringify({
@@ -23,15 +31,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                         },
                     }
                 )
-            ).data
+            )?.data
 
             if (!tokenGrantData) {
                 res.send({ success: false, error: 'Authentication failed' })
             } else {
-                res.send({ success: true, ...tokenGrantData })
+                res.setHeader('Set-Cookie', [
+                    serialize('token', JSON.stringify(tokenGrantData), {
+                        path: '/',
+                    }),
+                ])
+                res.redirect('/')
             }
         } catch (error) {
-            res.send({ success: false, ...error.response.data })
+            console.log(error)
+            res.send({ success: false, ...error.response?.data })
         }
     } else {
         res.send({ success: false, error: req.query.error })
