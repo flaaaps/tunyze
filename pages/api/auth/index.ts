8b4a2dd2749bf3df97b0eba52377a6ab36d4201e
flaaaps <julian@wening.me>
2021-07-28 13:31:50 +0200

    Add production url

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -1,7 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export const redirectURI =
-    process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/auth/callback' : ''
+    process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000/api/auth/callback'
+        : 'https://hardboun.de/api/auth/callback'
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
     res.redirect(
