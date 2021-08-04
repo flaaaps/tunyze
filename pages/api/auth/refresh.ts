2d98a0c09c29389517a30d05dd228fd219f77cc1
flaaaps <julian@wening.me>
2021-08-04 16:45:22 +0200

    Extract refresh process to seperate function

--- pages/api/auth/refresh.ts
+++ pages/api/auth/refresh.ts
@@ -9,27 +9,43 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!refreshToken) {
         res.send({ success: false, error: 'Refresh token missing' })
     } else {
-        const tokenGrantData = (
-            await axios.post(
-                'https://accounts.spotify.com/api/token',
-                qs.stringify({
-                    grant_type: 'refresh_token',
-                    refresh_token: refreshToken,
-                    redirect_uri: redirectURI,
-                    client_id: process.env.SPOTIFY_CLIENT_ID,
-                    client_secret: process.env.SPOTIFY_CLIENT_SECRET,
-                }),
-                {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                }
-            )
-        ).data
-        if (!tokenGrantData) {
-            res.send({ success: false, error: 'Authentication failed' })
-        } else {
-            res.send({ success: true, ...tokenGrantData })
-        }
+        const response = await refreshAccessToken(refreshToken as string)
+        res.json(response)
+    }
+}
+
+export interface RefreshedAuthData {
+    access_token: string
+    token_type: 'Bearer'
+    expires_in: number
+}
+
+export async function refreshAccessToken(
+    refreshToken: string
+): Promise<{ success: false; error: string } | ({ success: true } & RefreshedAuthData)> {
+    const tokenGrantData = (
+        await axios.post(
+            'https://accounts.spotify.com/api/token',
+            qs.stringify({
+                grant_type: 'refresh_token',
+                refresh_token: refreshToken,
+                redirect_uri: redirectURI,
+                client_id: process.env.SPOTIFY_CLIENT_ID,
+                client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }
+        )
+    ).data
+    console.log(tokenGrantData, 'TOKEN GRANT DATA!')
+    if (!tokenGrantData) {
+        // res.send({ success: false, error: 'Authentication failed' })
+        return { success: false, error: 'Authentication failed' }
+    } else {
+        // res.send({ success: true, ...tokenGrantData })
+        return { success: true, ...tokenGrantData }
     }
 }
