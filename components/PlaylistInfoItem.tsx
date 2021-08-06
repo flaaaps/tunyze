1d2cc910bee1be11541bf94590bfeff25bfae881
flaaaps <julian@wening.me>
2021-08-06 18:15:00 +0200

    Create component that shows playlist details

---
+++ components/PlaylistInfoItem.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface Props {
+    iconPath: string
+    alt: string
+    label: string | number
+}
+
+const PlaylistInfoItem: React.FC<Props> = ({ iconPath, alt, label }) => {
+    return (
+        <div className="flex items-center mr-5">
+            <div className="mr-1 h-5">
+                <Image src={`/images/${iconPath}`} alt={alt} width="20" height="20" />
+            </div>
+            <span>{label}</span>
+        </div>
+    )
+}
+
+export default PlaylistInfoItem
